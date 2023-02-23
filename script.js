12aef63a8d1f1cfe241a0a108b3933afaad894cc
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-02-23 11:33:47 +0600

    solved exercises 4

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 var arr=[
-  ["1", "2", "3", "4", "5", "6"],
- ["7", "8", "9", "10", "11", "12"],
- ["13", "14", "15", "16", "17", "18"], 
- ["19", "20", "21", "22", "23", "24"],
-  ["25", "26", "27", "28", "29", "30"]
-] ;
+  {one: 1, two: 2, three: 3, four: 4, five: 5, six: 6},
+  {one: 7, two: 8, three: 9, four: 10, five: 11, six: 12},
+  {one: 13, two: 14, three: 15, four: 16, five: 17, six: 18},
+  {one: 19, two: 20, three: 21, four: 22, five: 23, six: 24},
+  {one: 25, two: 26, three: 27, four: 28, five: 29, six: 30},
+  ] ;
+
+  var tb = document.getElementById("someTable");
+  var tr = document.getElementById("someTr");
+  var td = someTable.getElementsByTagName("td");
+  
+  var tdRank = someTable.getElementsByClassName("one");
+  var tdNick = someTable.getElementsByClassName("two");
+  var tdId = someTable.getElementsByClassName("three");
+  var tdRank = someTable.getElementsByClassName("four");
+  var tdNick = someTable.getElementsByClassName("five");
+  var tdId = someTable.getElementsByClassName("six");
+  
+  
+  for(var i=0; i<tdOne.length; i++){
+    tdOne[i].innerHTML = arr[i].one
+  };
+  
+  for(var i=0; i<tdTwo.length; i++){
+    tdTwo[i].innerHTML = arr[i].two
+  };
+  
+  for(var i=0; i<tdThree.length; i++){
+    tdThree[i].innerHTML = arr[i].three
+  };
+  for(var i=0; i<tdFour.length; i++){
+    tdFour[i].innerHTML = arr[i].four
+  };
+  
+  for(var i=0; i<tdFive.length; i++){
+    tdFive[i].innerHTML = arr[i].five
+  };
+  
+  for(var i=0; i<tdSix.length; i++){
+    tdSix[i].innerHTML = arr[i].six
+  };
